fe87dec1aedc4b91c39088afb427c4637fbf400b
faerietree <radagast@ciry.at>
2017-07-09 22:42:50 +0200

    Add convenience access for left, normal, right, border, news content.

---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,19 @@
+# <INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/setup.ts">
+styles.content {
+
+   # get content, left
+   getLeft < styles.content.get
+   getLeft.select.where = {#colPos}=1
+
+   # get content, right
+   getRight < styles.content.get
+   getRight.select.where = {#colPos}=2
+
+   # get content, border
+   getBorder < styles.content.get
+   getBorder.select.where = {#colPos}=3
+
+   # get news
+   getNews < styles.content.get
+   getNews.select.pidInList = {$styles.content.getNews.newsPid}
+}
